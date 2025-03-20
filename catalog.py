fa04e042ca90881cce67c1c3bef8643b62282bc3
Hayden Taylor <h.taylor2106@gmail.com>
2025-03-19 22:32:33 -0400

    add some unknots

--- catalog.py
+++ catalog.py
@@ -1,12 +1,12 @@
 from diagram import Diagram
 
 # Initialize pre-defined prime knot knot diagrams
-knots = {}
+_knot_catalog = {}
 with open('knotinfo.csv') as f:
     for line in f.readlines():
         parts = line.split(',')
         name, raw_pd = parts[0], parts[1]
-        knots[name] = raw_pd
+        _knot_catalog[name] = raw_pd
 
 def knot(crossings: int, index: int, alt_status: str = ''):
     if alt_status == '' and crossings > 10:
@@ -17,7 +17,7 @@ def knot(crossings: int, index: int, alt_status: str = ''):
     name = f'{crossings}{alt_status}_{index}'
     
     try:
-        raw_pd = knots[name]
+        raw_pd = _knot_catalog[name]
     except KeyError:
         raise KeyError(f"knot {name} does not exist.")
 
@@ -28,3 +28,24 @@ def knot(crossings: int, index: int, alt_status: str = ''):
         pd.add(
             (ns[4*i], ns[4*i+1], ns[4*i+2], ns[4*i+3]))
     return Diagram(pd)
+
+# https://en.m.wikipedia.org/wiki/File:Thistlethwaite_unknot.svg
+THISTLETHWAITE_UNKNOT = Diagram([
+    (22, 2, 23, 1),   (3, 27, 4, 26),   (5, 21, 6, 20),
+    (7, 18, 8, 19),   (9, 25, 10, 24),  (11, 2, 12, 3),
+    (4, 13, 5, 14),   (15, 6, 16, 7),   (30, 17, 1, 18),
+    (19, 14, 20, 15), (28, 22, 29, 21), (23, 11, 24, 10),
+    (25, 9, 26, 8),   (27, 12, 28, 13), (16, 29, 17, 30)
+])
+
+# https://en.m.wikipedia.org/wiki/File:Ochiai_unknot.svg
+OCHIAI_UNKNOT = Diagram([
+    (12, 2, 13, 1),   (3, 6, 4, 7),     (5, 24, 6, 25),
+    (7, 15, 8, 14),   (16, 10, 17, 9),  (26, 12, 1, 11),
+    (13, 20, 14, 21), (22, 15, 23, 16), (10, 18, 11, 17),
+    (19, 3, 20, 2),   (21, 8, 22, 9),   (23, 4, 24, 5),
+    (25, 19, 26, 18)
+])
+
+# A simple unknot with a twist move.
+INFINITY_UNKNOT = Diagram([(1, 1, 2, 2)])
