e31df56a92f663de2f4a70be618dc996bd0189fd
Hayden Taylor <h.taylor2106@gmail.com>
2025-03-21 10:30:42 -0400

    start test file

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from catalog import *
+from diagram import Diagram
+from polynomial import KnotPoly
+
+class TestKauffmanBracket(unittest.TestCase):
+    def test_unknot_bracket(self):
+        unknot = knot(0, 1)
+        expected_polynomial = KnotPoly.univariate({0: 1})
+        self.assertEqual(unknot.get_kauffman_bracket(), expected_polynomial)
+
+if __name__ == '__main__':
+    unittest.main()
