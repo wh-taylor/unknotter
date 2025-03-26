fc346cb0a51392bcbdcdf021fe9ee57c363d5c3b
Hayden Taylor <h.taylor2106@gmail.com>
2025-03-26 15:24:07 -0400

    place source files in subdirectory

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,7 @@
 import unittest
-from catalog import *
-from diagram import Diagram, ReidemeisterError
-from polynomial import KnotPoly
+from knotdiagram.catalog import *
+from knotdiagram.diagram import Diagram, ReidemeisterError
+from knotdiagram.polynomial import KnotPoly
 
 if __name__ == '__main__':
     unittest.main()
