8de9e40d1e8b937dd27492f36ed426776628e62c
Hayden Taylor <h.taylor2106@gmail.com>
2025-07-13 17:51:34 -0400

    add size restraint on csv reader

--- unknotter/csvreader.py
+++ unknotter/csvreader.py
@@ -1,10 +1,11 @@
 from unknotter.catalog import _raw_pd_to_pd
 from unknotter.diagram import Diagram
 
-def read_to_list(filename: str):
+def read_to_list(filename: str, count: int = -1):
     catalog: list[tuple[str, str]] = []
     with open(filename) as f:
-        for line in f.readlines()[1:]:
+        lines = f.readlines()[1:count+1] if count >= 0 else f.readlines()[1:]
+        for line in lines:
             parts = line.split(',')
             name, raw_pd = parts[0], parts[1]
             catalog.append((name, _raw_pd_to_pd(raw_pd)))
